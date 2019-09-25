8602d733a3eb2142439f4290355d5b2a666f227c
Moisés Sánchez <moy.sav21@hotmail.com>
2019-09-25 01:38:07 -0500

    Agregar archivos de proyecto.

---
+++ FormRestaurante/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace FormRestaurante
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nuevoRegistro = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.forgotPassword = new System.Windows.Forms.LinkLabel();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnOmitir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(198, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOMBRE";
+            // 
+            // nuevoRegistro
+            // 
+            this.nuevoRegistro.AutoSize = true;
+            this.nuevoRegistro.Location = new System.Drawing.Point(303, 215);
+            this.nuevoRegistro.Name = "nuevoRegistro";
+            this.nuevoRegistro.Size = new System.Drawing.Size(60, 13);
+            this.nuevoRegistro.TabIndex = 1;
+            this.nuevoRegistro.TabStop = true;
+            this.nuevoRegistro.Text = "Registrarse";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CONTRASEÑA";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(306, 64);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(169, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(306, 119);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(169, 20);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // forgotPassword
+            // 
+            this.forgotPassword.AutoSize = true;
+            this.forgotPassword.Location = new System.Drawing.Point(275, 176);
+            this.forgotPassword.Name = "forgotPassword";
+            this.forgotPassword.Size = new System.Drawing.Size(131, 13);
+            this.forgotPassword.TabIndex = 5;
+            this.forgotPassword.TabStop = true;
+            this.forgotPassword.Text = "¿Ólvidaste tu contraseña?";
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(220, 276);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 6;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // btnOmitir
+            // 
+            this.btnOmitir.Location = new System.Drawing.Point(400, 276);
+            this.btnOmitir.Name = "btnOmitir";
+            this.btnOmitir.Size = new System.Drawing.Size(75, 23);
+            this.btnOmitir.TabIndex = 7;
+            this.btnOmitir.Text = "Omitir";
+            this.btnOmitir.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FormRestaurante.Properties.Resources.Portada;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(682, 450);
+            this.Controls.Add(this.btnOmitir);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.forgotPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nuevoRegistro);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Alitas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel nuevoRegistro;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.LinkLabel forgotPassword;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnOmitir;
+    }
+}
+
